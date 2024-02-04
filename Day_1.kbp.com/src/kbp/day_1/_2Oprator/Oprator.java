ab756aee70a3f678e5c3e8578d44c8efa0c93c55
Pratik Sambhaji Pawar <129174056+pratikspawar@users.noreply.github.com>
2024-02-04 18:56:17 +0530

    Add files via upload

---
+++ Day_1.kbp.com/src/kbp/day_1/_2Oprator/Oprator.java
@@ -0,0 +1,62 @@
+package kbp.day_1._2Oprator;
+
+public class Oprator {
+
+public static void main(String[] args) {
+	
+	// Arithmetic( +,-,*/,%)
+	/*
+	int a=10;
+	int b=11;System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a*b);
+	System.out.println(a/b);
+	System.out.println(a%b);
+	*/
+	
+	//increment decrement (++,--)
+	/*
+	int a=10;
+	int b=a++;
+	System.out.println(a);
+	System.out.println(b);
+	int g= 10;
+	int h=++g;
+	System.out.println(g);
+	System.out.println(h);
+	*/
+	
+	
+	//String concatanation (+)
+	//System.out.println(10+39+"a bc"+"Wxy"+10+10);
+	
+	//Relational operator < ,>,==
+	/*
+	int a=10;
+	int b=11;
+	System.out.println(a>b);
+	System.out.println(a<b);
+	System.out.println(a==b);
+	*/
+	
+	//Bitwise (&,|,>>,<<) 
+	/*
+	System.out.println(true&false);
+	System.out.println(true|false);
+	*/
+	
+	//Short circuit (&&,||)
+	/*
+	System.out.println(true&&false);// if first argument is false then it not check second argument
+	System.out.println(true||false);//  if first argument is  true then it not check second argument
+   */
+ 
+	//Ternary operator
+	 /*
+	 int a=10;
+	 int b=11;
+	 String d=(a>b) ? "First is big " :"Second is big ";
+	 System.out.println(d);
+	 */
+	}
+}
