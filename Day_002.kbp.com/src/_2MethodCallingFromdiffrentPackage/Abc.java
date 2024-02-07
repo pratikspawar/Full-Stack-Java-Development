647183df9dd8f3ff317fb73122c3a9d4b4f9ffa9
Pratik Sambhaji Pawar <129174056+pratikspawar@users.noreply.github.com>
2024-02-07 10:48:11 +0530

    Delete Day_002.kbp.com directory

--- Day_002.kbp.com/src/_2MethodCallingFromdiffrentPackage/Abc.java
+++
@@ -1,9 +0,0 @@
-package _2MethodCallingFromdiffrentPackage;
-
-import _1StaticMethods.A;
-
-
-
-public class Abc {
-A obj=new A();
-}
--- Day_002.kbp.com/src/_4Constructor/Red.java
+++
@@ -1,8 +0,0 @@
-package _4Constructor;
-
-
-public class Red {
-	   public static void main(String[] args) {
-		   Constructor obj =new Constructor();
-	}
-	}
