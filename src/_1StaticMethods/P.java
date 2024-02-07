66fb3592228811c086987b604c47a01ca67f80e3
Pratik Sambhaji Pawar <129174056+pratikspawar@users.noreply.github.com>
2024-02-07 10:49:35 +0530

    Add files via upload

---
+++ src/_1StaticMethods/P.java
@@ -0,0 +1,10 @@
+package _1StaticMethods;
+
+public class P {
+	  static {
+		  System.out.println("Static block ");
+	  }
+	  {
+		  System.out.println("Non static block");
+	  }
+	}
---
+++ src/_2MethodCalling/D.java
@@ -0,0 +1,10 @@
+package _2MethodCalling;
+
+public class D {
+	 public void m1() {
+		 System.out.println("Inside the  class B in m2 method");
+	 }
+	 public void m2() {
+		 System.out.println("Inside the class  B in m2 method");
+	 }
+}
