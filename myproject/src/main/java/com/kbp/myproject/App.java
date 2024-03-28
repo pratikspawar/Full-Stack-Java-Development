8750ee6fdbb0faa4ad3ccf1dfded937838b81fe6
Pratik Sambhaji Pawar <129174056+pratikspawar@users.noreply.github.com>
2024-03-28 16:45:48 +0530

    Add files via upload

---
+++ myproject/src/main/java/com/kbp/myproject/App.java
@@ -0,0 +1,13 @@
+package com.kbp.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       
+    }
+}
---
+++ myproject/src/main/java/com/kbp/myproject/Library.java
@@ -0,0 +1,5 @@
+package com.kbp.myproject;
+
+public class Library {
+
+}
